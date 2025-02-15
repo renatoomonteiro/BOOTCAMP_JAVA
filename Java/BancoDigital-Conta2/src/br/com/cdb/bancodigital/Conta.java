c52d64f95e15cab1c3f1263eba04e8d9f8de2975
Renato Monteiro <reoliveiramonteiro.rm@gmail.com>
2025-02-15 07:59:19 -0300

    Atualização de BancoDigital-Conta2
    Atualização do repositório para que sejam feitos commits via Git Hub Desktop

---
+++ Java/BancoDigital-Conta2/src/br/com/cdb/bancodigital/Conta.java
@@ -0,0 +1,29 @@
+package br.com.cdb.bancodigital;
+
+public class Conta {
+	// private = modificador de acesso
+		private int numero;
+		private double saldo;
+		
+		public Conta(int numero) {
+			this.numero = numero;
+			this.saldo = 0;				
+		}
+
+		public int getNumero() {
+			return numero;
+		}
+
+		public void setNumero(int numero) {
+			this.numero = numero;
+		}
+
+		public double getSaldo() {
+			return saldo;
+		}
+
+		public void depositar(double saldo) { 
+			this.saldo = saldo;
+		}
+
+}
