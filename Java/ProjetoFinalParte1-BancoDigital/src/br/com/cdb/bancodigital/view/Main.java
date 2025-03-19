88a47c54c4ee16fc12949e458199b551feb458d1
Renato Monteiro <reoliveiramonteiro.rm@gmail.com>
2025-03-19 20:29:30 -0300

    Teste commit casa Main
    Projeto final Parte 1

--- Java/ProjetoFinalParte1-BancoDigital/src/br/com/cdb/bancodigital/view/Main.java
+++ Java/ProjetoFinalParte1-BancoDigital/src/br/com/cdb/bancodigital/view/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 
 		Menu menuPrincipal = new Menu();
-		
-
+		//TODO -> Editar as mensagens de retorno, validações e o restante, etc. de acordo com o enunciado 
+		//TODO - > Validar pesquisa de cliente por CPF quando este for inválido
 	}
 
 }
